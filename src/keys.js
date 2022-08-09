ad8618784febbd0b0838113225fddee4cecfc6df
Adan Sanchez Hernandez <adansaherr30dev@gmail.com>
2022-08-09 00:41:57 -0500

    .

--- src/keys.js
+++ src/keys.js
@@ -3,7 +3,7 @@ module.exports={
     database:{
         host:'localhost',
         user:'root',
-        password:'30102000',
+        password:'201264',
         database:'database_links'
     }
 };
